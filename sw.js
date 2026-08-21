d75901a7e5d3da998692c5480e99ca34258ac817
javierghali <javier.ghali.jg@gmail.com>
2026-08-21 19:36:51 +0700

    Bump cache for duplicate-safe importer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="silverback-v3-cache-v5";
+const CACHE="silverback-v3-cache-v6";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./importer.js","./bulk.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
