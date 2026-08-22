ebec737a10bc1b6b76e1715359346dfab7abb281
javierghali <javier.ghali.jg@gmail.com>
2026-08-23 00:47:50 +0700

    Load Supabase auth compatibility fix

--- status-bulk.js
+++ status-bulk.js
@@ -45,6 +45,7 @@
 
   cleanupBackupUi();
   loadModule('./config-defaults.js','configDefaultsModule');
+  loadModule('./auth-fix.js','authFixModule');
   loadModule('./bulk-delete.js','bulkDeleteModule');
   loadModule('./navigation.js','navigationModule');
   loadModule('./bulk-group.js','bulkGroupModule');
