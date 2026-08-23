a6a7253a1956b84f1479effd83a643991d4144bf
javierghali <javier.ghali.jg@gmail.com>
2026-08-23 10:59:17 +0700

    Bump cache after restoring Owners splitter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="silverback-v3-cache-v19";
+const CACHE="silverback-v3-cache-v20";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./importer.js","./bulk.js","./groups.js","./status.js","./status-bulk.js","./auth-fix.js","./bulk-delete.js","./navigation.js","./bulk-group.js","./paste-export.js","./bulk-username.js","./owners-splitter.js","./value-tracker.js","./data/recent-values.json","./config-defaults.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
